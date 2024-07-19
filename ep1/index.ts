f333377be7fc76cdc31c4d16707d08f428c03187
oBrighTo9 <brightpiyapat01@gmail.com>
2024-07-19 11:40:14 +0700

    ep2

--- ep1/index.ts
+++ ep1/index.ts
@@ -1,1 +1,29 @@
 const fullName: string = 'ปิยาพัชร ทองบุญ'
+
+
+var x: = 5 
+var y: = 10
+
+var msg = 'hello'
+var isperson = false
+
+console.log (x)
+console.log (y)
+console.log (msg)
+console.log (isperson)
+
+const a: number = 10
+const b: number = 15
+
+console.log (a === b)
+console.log (a > b)
+console.log (a <= b)
+
+let a: number = 10
+let b: number = 15
+
+console.log (a === b) 
+console.log (a < b)
+console.log(a <= b )
+
+
