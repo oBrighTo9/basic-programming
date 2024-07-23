eba8be607317299f9f277032f5dfedcd13588287
oBrighTo9 <brightpiyapat01@gmail.com>
2024-07-23 11:52:44 +0700

    เรียน function

---
+++ ep2/index.ts
@@ -0,0 +1,25 @@
+function add(a: number, b: number) {
+     return a - b
+}
+
+console.log (add(20, 10))
+
+function boom (a: number, b: number, c: number) {
+ return a * b * c
+
+}
+
+console.log (boom(2, 2,5 ))
+function grade(homework: number, midterm: number, final: number){
+ let score: number = homework + midterm + final
+ if (score < 50) {
+    return 'เกรด F'
+ } else if (score < 60){
+ return "เกรด D"
+}else if (score < 70){
+} return "เกรด c"
+} 
+
+
+
+console.log(grade(500,250,250))
