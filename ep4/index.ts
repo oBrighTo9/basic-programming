54c071f0a0e802cc0ee38bdec5b391257fe2dc33
oBrighTo9 <brightpiyapat01@gmail.com>
2024-08-02 11:46:06 +0700

    ep4

---
+++ ep4/index.ts
@@ -0,0 +1,18 @@
+//function fixNumber (x: number) {
+    //const y = x.toLocaleString()
+   // return y
+ //}
+ //console.log (fixNumber(3000000))
+
+
+ //const str: string = "Piyapat How are you" 
+//console.log(str.length)
+
+//const str: string = "Hello" 
+//console.log(str.charAt(0)) // ที่นับตัวอักษร
+
+//const str: string = "ปิยาพัชร ทองบุญ"  //เป็นที่นับตัวเลข
+//console.log(str.length) 
+
+// const str: string = "Piyapat" 
+//console.log(str.indexOf('i')) //ไว้นับชื่อตัวเอง
